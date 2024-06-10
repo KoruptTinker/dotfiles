18a353f89a57e30df3a603132c6c5b9c5a230273
KoruptTinker <pisces1237@gmail.com>
2024-06-10 23:04:35 +0530

    Add: Telescope Keymappings

--- nvim/lua/plugins/root.lua
+++ nvim/lua/plugins/root.lua
@@ -23,7 +23,7 @@ return {
   -- change trouble config
   {
     "folke/trouble.nvim",
-    -- opts will be merged with the parent spec
+    -- oexaswill be merged with the parent spec
     opts = { use_diagnostic_signs = true },
   },
 
@@ -43,16 +43,6 @@ return {
   -- change some telescope options and a keymap to browse plugin files
   {
     "nvim-telescope/telescope.nvim",
-    keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
-      {
-        "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-        desc = "Find Plugin File",
-      },
-    },
-    -- change some options
     opts = {
       defaults = {
         layout_strategy = "horizontal",
