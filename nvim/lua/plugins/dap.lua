854efbb4578aa8e655f0519aec95db0d9e235122
KoruptTinker <pisces1237@gmail.com>
2024-06-27 19:09:47 +0530

    Add: Golang support

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -26,6 +26,38 @@ return {
           port = 5005,
         },
       }
+
+      dap.adapters.delve = {
+        type = "server",
+        port = "${port}",
+        executable = {
+          command = "dlv",
+          args = { "dap", "-l", "127.0.0.1:${port}" },
+        },
+      }
+
+      dap.configurations.go = {
+        {
+          type = "delve",
+          name = "Debug",
+          request = "launch",
+          program = "${file}",
+        },
+        {
+          type = "delve",
+          name = "Debug test",
+          request = "launch",
+          mode = "test",
+          program = "${file}",
+        },
+        {
+          type = "delve",
+          name = "Debug test (go.mod)",
+          request = "launch",
+          mode = "test",
+          program = "./${relativeFileDirname}",
+        },
+      }
     end,
   },
 }
